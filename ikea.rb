7c26a3c09847e0ad17678fbbb3586bebc040a972
Max <silverhammermba@gmail.com>
2016-11-08 14:15:43 -0500

    simplify by considering everything as a digraph
    lengths can get out of hand now

--- ikea.rb
+++ ikea.rb
@@ -2,9 +2,6 @@ require 'set'
 
 class Predictor
   def initialize source
-    @initial_counts = Hash.new 0
-    @length_counts = Hash.new 0
-    @monograph_counts = {}
     @digraph_counts = {}
 
     @sources = Set.new
@@ -15,57 +12,42 @@ class Predictor
         next if @sources.include? name
         @sources << name
 
-        @initial_counts[name[0]] += 1
-        @length_counts[name.length] += 1
-        (0..(name.length - 2)).each do |i|
-          @monograph_counts[name[i]] ||= Hash.new 0
-          @monograph_counts[name[i]][name[i + 1]] += 1
-        end
+        # first letter
+        @digraph_counts["^^"] ||= Hash.new 0
+        @digraph_counts["^^"][name[0]] += 1
+
+        first = "^#{name[0]}"
+        @digraph_counts[first] ||= Hash.new 0
+        @digraph_counts[first][name[1]] += 1
+
         (0..(name.length - 3)).each do |i|
           digraph = name[i..(i+1)]
           @digraph_counts[digraph] ||= Hash.new 0
           @digraph_counts[digraph][name[i + 2]] += 1
         end
+
+        last = name[-2..-1]
+        @digraph_counts[last] ||= Hash.new 0
+        @digraph_counts[last][nil] += 1
       end
     end
 
-    # transform length_counts into a cumulative distribution
-    @max_length = @length_counts.keys.max
-    @max_length.downto(0).each do |i|
-      (0...i).each do |j|
-        @length_counts[i] += @length_counts[j]
-      end
-    end
+    @totals = @digraph_counts.map { |d, n| [d, n.values.reduce(:+)] }.to_h
 
-    @totals = {}
-    @totals[@length_counts] = @length_counts[@max_length]
-    @totals[@initial_counts] = @initial_counts.values.reduce(:+)
-    @totals[@monograph_counts] = @monograph_counts.map { |m, n| [m, n.values.reduce(:+)] }.to_h
-    @totals[@digraph_counts] = @digraph_counts.map { |d, n| [d, n.values.reduce(:+)] }.to_h
+    p @sources.max_by { |x| x.length }
   end
 
   # predict the next character to be added to the string
   # or nil if the string is long enough
   private def predict_next str
-    # simple case of word already being too long
-    return nil if str.length >= @max_length
-
-    # test length of word to determine ending
-    # TODO choose last letter of word based on source?
-    r = rand(@totals[@length_counts])
-    return nil if r < @length_counts[str.length]
-
-    case str.length
-    when 0
-      counts = @initial_counts
-      total = @totals[@initial_counts]
-    when 1
-      counts = @monograph_counts[str[-1]]
-      total = @totals[@monograph_counts][str[-1]]
-    else
-      counts = @digraph_counts[str[-2..-1]]
-      total = @totals[@digraph_counts][str[-2..-1]]
+    while str.length < 2
+      str = "^#{str}"
     end
+
+    di = str[-2..-1]
+
+    counts = @digraph_counts[di]
+    total = @totals[di]
 
     # TODO try different counts if nil?
     return nil unless counts
